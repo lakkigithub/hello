0a0d2f5b024a0a5edc5543d00a5a31054c39516e
lakkigithub <46402736+lakkigithub@users.noreply.github.com>
2019-01-07 12:33:33 -0500

    Update employeeInfo.java

--- src/employeeInfo.java
+++ src/employeeInfo.java
@@ -4,9 +4,7 @@ public static void main(String args[])
 
 
 {
-	
-	
-	
+
 	System.out.println("employee name is lakshmi.");
 	System.out.println("gender is female");
 	System.out.println("employee id is 666");
